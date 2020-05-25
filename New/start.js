8656f7f52613a67f81fdd958804565e6fccfafc5
yauheni <zhenyaveremchuk@gmail.com>
2020-05-25 16:26:45 +0300

    cycles DO, WHILE, FOR

--- New/start.js
+++ New/start.js
@@ -15,12 +15,59 @@ let personalMovieDB = {
     privat: false 
 };
 
-let a = prompt("Один из последних просмотренных фильмов?","");
-let b = prompt("Насколько оцените его?","");
-let c = prompt("Один из последних просмотренных фильмов?","");
-let d = prompt("Насколько оцените его?","");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+//РЕШЕНИЕ ЧЕРЕЗ FOR
+
+for (let i=0; i<2; i++) {                                                       
+    let a = prompt("Один из последних просмотренных фильмов?","");
+    let b = prompt("Насколько оцените его?","");    
+
+    if(a ==  "" || a == null || a.length > 50 ||b ==  "" || b == null || b.length > 50 ){
+        i--;
+    } else{
+        personalMovieDB.movies[a] = b;
+    }    
+}
+
+// РЕШЕНИЕ ЧЕРЕЗ WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Один из последних просмотренных фильмов?", "");
+//     let b = prompt("Насколько оцените его?", "");
+//     i++;
+
+//     if (a == "" || a == null || a.length > 50 || b == "" || b == null || b.length > 50) {
+//         i--;
+//     } else {
+//         personalMovieDB.movies[a] = b;
+//     }
+
+// }
+
+// РЕШЕНИЕ ЧЕРЕЗ DO
+// let i = 0;
+// do {
+//     let a = prompt("Один из последних просмотренных фильмов?", "");
+//     let b = prompt("Насколько оцените его?", "");
+//     i++;
+
+//     if (a == "" || a == null || a.length > 50 || b == "" || b == null || b.length > 50) {
+//         i--;
+//     } else {
+//         personalMovieDB.movies[a] = b;
+//     }
+// }
+// while (i < 2);
+
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Что-то пошло не так");
+}
