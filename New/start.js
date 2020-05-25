2003a388bd008d946d10e851e9b9dd722555c541
yauheni <zhenyaveremchuk@gmail.com>
2020-05-25 12:35:58 +0300

    Update for begin

--- New/start.js
+++ New/start.js
@@ -1,18 +1,6 @@
 "use strict";
-
-
 // const category = "toys";
-
 // console.log(`https://someurl.com/${category}/5/123123`);
 
 // const user = "ivan";
-
 // alert(`Привет, ${user}`);
-
-let incr = 10,
-    decr = 10;
-
-incr++
-decr
-console.log(incr);
-console.log(decr);
